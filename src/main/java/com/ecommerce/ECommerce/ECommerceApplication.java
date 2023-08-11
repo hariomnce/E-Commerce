849e566a1f15ecbd136805b077040f4374137631
Hariom Sharma <hariomne@gmail.com>
2023-08-11 18:32:02 +0530

    Code update.

--- src/main/java/com/ecommerce/ECommerce/ECommerceApplication.java
+++ src/main/java/com/ecommerce/ECommerce/ECommerceApplication.java
@@ -1,39 +1,13 @@
 package com.ecommerce.ECommerce;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableScheduling
-@OpenAPIDefinition
-        (info = @Info(
-                title = "E-Commerce Web Applicaion API",
-                version = "2.0",
-                description = "E-Commerce Setup Module Documentation"
-        ))
 public class ECommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ECommerceApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
-            }
-        };
-    }
 }
